86d220b1907d51b9e3583b39c31123fa4b060f6e
ZarbaliyevOzal <34451434+ZarbaliyevOzal@users.noreply.github.com>
2022-07-11 13:05:44 +0400

    removed unused comments

--- index.php
+++ index.php
@@ -1,48 +1,4 @@
 <?php
-
-// if (!isset($argc)) {
-//   die("argc and argv disabled");
-//   exit();
-// }
-
-// if (!isset($argv[1])) {
-//   die('Please provide input file');
-//   exit();
-// }
-
-// $inputFile = $argv[1];
-
-// echo $inputFile;
-
-
-// public function readCsv($filepath)
-// {
-//     $arr = []; 
-
-//     // Open the file for reading
-//     if (($h = fopen("{$filepath}", "r")) !== FALSE) 
-//     {
-//         while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
-//         {
-//             // Each individual array is being pushed into the nested array
-//             $arr[] = [
-//                 'user_id' => $data[1],
-//                 'user_type' => $data[2],
-//                 'operation_type' => $data[3],
-//                 'operation_amount' => $data[4],
-//                 'currency' => $data[5],
-//                 'date' => $data[0]
-//             ];		
-//         }
-
-//         // Close the file
-//         fclose($h);
-//     }
-
-//     // Display the code in a readable format
-//     return $arr;
-// }
-
 require 'app/bootstrap.php';
 
 use App\Models\Operation;
